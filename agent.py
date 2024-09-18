1818fd3224d83ec038fbdfe510facccb5d28e08a
franzele21 <muller.francois.7@gmail.com>
2024-09-18 17:55:44 +0200

    nouvelle collision + demande à l'env du voisinage

--- agent.py
+++ agent.py
@@ -10,30 +10,46 @@ class Agent():
     def __init__(self, pos_x, pos_y, 
                  dir_x=np.random.choice([-1, 0, 1]), 
                  dir_y=np.random.choice([-1, 0, 1]), 
-                 couleur=list(np.random.choice(range(256), size=3))
+                 couleur=None
                  ) -> None:
         self.pos_x = pos_x
         self.pos_y = pos_y
+        self.pos_proch_x = pos_x
+        self.pos_proch_y = pos_x
         self.dir_x = dir_x
         self.dir_y = dir_y
-        self.couleur = couleur
-        self.trajectoire_changee = False
+        if isinstance(couleur, type(None)):
+            color = [str(x) for x in range(10)] + ["a", "b", "c", "d", "e", "f"]
+            self.couleur = "#"+"".join(np.random.choice(color, size=3))
+        else:
+            self.couleur = couleur
     
     def __repr__(self):
         return f" |{self.pos_x} {self.pos_y}| "
+    
+    def predit(self):
+        self.pos_proch_x += self.dir_x
+        self.pos_proch_y += self.dir_y
 
     def update(self):
-        self.pos_x += self.dir_x
-        self.pos_y += self.dir_y
+        self.pos_x = self.pos_proch_x
+        self.pos_y = self.pos_proch_y
 
-    def decide(self, agent):
-        if agent != self:
-            if agent.dir_x == 0 and agent.dir_y == 0:
-                agent.dir_x = self.dir_x
-                agent.dir_y = self.dir_y
-                self.dir_x = -self.dir_x
-                self.dir_y = -self.dir_y
-            else:
-                self.dir_x, agent.dir_x = agent.dir_x, self.dir_x
-                self.dir_y, agent.dir_y = agent.dir_y, self.dir_y
+    def decide(self, env):
+        agent_liste = env.voisinage(self.pos_x, self.pos_y, 1)
+        if agent_liste:
+            for agent in agent_liste:
+                if agent != self:
+                    if agent.pos_x == self.pos_x+self.dir_x \
+                            and agent.pos_y == self.pos_y+self.dir_y \
+                            and agent.pos_y != self.pos_y \
+                            or agent.pos_x != self.pos_x:
+                        if agent.dir_x == 0 and agent.dir_y == 0:
+                            agent.dir_x = self.dir_x
+                            agent.dir_y = self.dir_y
+                            self.dir_x = -self.dir_x
+                            self.dir_y = -self.dir_y
+                        else:
+                            self.dir_x, agent.dir_x = agent.dir_x, self.dir_x
+                            self.dir_y, agent.dir_y = agent.dir_y, self.dir_y
         
