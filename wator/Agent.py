f9776e7efb3ec0b54a6fc42a99d6ffea4c585eb0
franzele <muller.francois.7@gmail.com>
2024-09-20 14:50:57 +0200

    roooooooooooooooooooooooooh pierre ptn

--- wator/Agent.py
+++ wator/Agent.py
@@ -7,12 +7,11 @@ class Poisson(GenericAgent):
     def decide(self, environement):
         # Si possible, s'éloigne des requins
         pass
-
-    def get_color(self):
-        return self.color
     
 class Requin(GenericAgent):
     def __init__(self, pos_x, pos_y, color="red", trace=False):
         super().__init__(pos_x, pos_y, color, trace)
-    
-    
+
+    def decide(self, environnement):
+        # Si possible, bouffe les poissons    
+        pass
